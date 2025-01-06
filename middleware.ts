cc44d2f2c95c5190ed06ec04762df0e4d5dd55a1
Luis <luis.iannello3@gmail.com>
2025-01-06 18:08:31 +0100

    second commit

--- middleware.ts
+++ middleware.ts
@@ -1,55 +1,21 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
-
-// Definir rutas públicas
-const isPublicRoute = createRouteMatcher([
-  '/',
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/events/:id',
-  '/api/webhook/clerk',
-  '/api/webhook/stripe',
-  '/api/uploadthing'
-]);
-
-export default clerkMiddleware((auth, request) => {
-  const url = request.nextUrl;
-
-  // Permitir acceso a rutas públicas
-  if (isPublicRoute(request)) {
-    return NextResponse.next();
-  }
-
-  // Proteger rutas privadas
-  return auth.isSignedIn
-    ? NextResponse.next()
-    : NextResponse.redirect(new URL('/sign-in', url.origin));
-});
-
-export const config = {
-  matcher: [
-    // Omitir archivos internos y estáticos
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Ejecutar siempre en rutas de API
-    '/(api|trpc)(.*)',
-    // Añadir rutas ignoradas
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
     '/api/webhook/clerk',
     '/api/webhook/stripe',
     '/api/uploadthing'
   ]
+});
+ 
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
-
-
-
-// publicRoutes: [
-//   '/',
-//   '/events/:id',
-//   '/api/webhook/clerk',
-//   '/api/webhook/stripe',
-//   '/api/uploadthing'
-// ], 
-// ignoredRoutes: [
-//   '/api/webhook/clerk',
-//   '/api/webhook/stripe',
-//   '/api/uploadthing'
-// ]
+ 

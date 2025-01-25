257d022bf2c906be0dbd90470330b942cbb7862b
Luis <luis.iannello3@gmail.com>
2025-01-25 14:29:14 +0100

    third commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,29 +2,30 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached =(global as any).mongoose || {conn: null, promise: null};
+if (!MONGODB_URI) {
+    throw new Error(
+        'Please define the MONGODB_URI environment variable inside .env.local'
+    );
+}
+
+interface MongooseCache {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+}
+
+let cached: MongooseCache = (global as any).mongoose || { conn: null, promise: null };
+(global as any).mongoose = cached;
 
 export const connectToDatabase = async () => {
     if (cached.conn) return cached.conn;
 
-    if (!MONGODB_URI) {
-        throw new Error(
-            'Please define the MONGODB_URI environment variable inside .env.local'
-        );
-
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI!, {
-        dbName: 'evnto',
-        bufferCommands: false,
-    })
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, {
+            dbName: 'evnto',
+            bufferCommands: false,
+        });
+    }
 
     cached.conn = await cached.promise;
     return cached.conn;
-    }
-}
-
-//Server Actions
-//connectToDatabase() ....
-
-
-
-// Continuar desde aca 23/12
+};
